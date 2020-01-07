3def6c48a26f779ed999ef802044c0a513e3205a
Arnaud Barré <arnaud.barre72@gmail.com>
2020-01-07 20:36:11 +0100

    Update types (#44)

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,29 +1,34 @@
-import React from 'react';
-import { StyleProp, TextStyle } from 'react-native';
+import { PureComponent } from 'react';
+import { StyleProp, TextStyle, TextProps } from 'react-native';
+import { Match } from 'autolinker';
 
 export interface AutolinkProps {
   email?: boolean;
-  hashtag?: false|'facebook'|'instagram'|'twitter';
+  hashtag?: false | 'facebook' | 'instagram' | 'twitter';
   latlng?: boolean;
   linkStyle?: StyleProp<TextStyle>;
-  mention?: false|'instagram'|'soundcloud'|'twitter';
-  onPress?: (url: string, match: string) => void;
-  onLongPress?: (url: string, match: string) => void;
-  phone?: boolean|string;
-  renderLink?: (text: string, match: string, index: number) => void;
+  mention?: false | 'instagram' | 'soundcloud' | 'twitter';
+  onPress?: (url: string, match: Match) => void;
+  onLongPress?: (url: string, match: Match) => void;
+  phone?: boolean | 'text';
+  renderLink?: (text: string, match: Match, index: number) => void;
   showAlert?: boolean;
   stripPrefix?: boolean;
   stripTrailingSlash?: boolean;
-  style?: StyleProp<TextStyle>;
   text: string;
   truncate?: number;
   truncateChars?: string;
-  truncateLocation?: 'end'|'middle'|'smart';
+  truncateLocation?: 'end' | 'middle' | 'smart';
+  /**
+   * @deprecated Use mention
+   */
   twitter?: boolean;
-  url?: boolean | { schemeMatches?: boolean; wwwMatches?: boolean; tldMatches?: boolean; };
+  url?:
+      | boolean
+      | { schemeMatches?: boolean; wwwMatches?: boolean; tldMatches?: boolean };
   webFallback?: boolean;
 }
 
-export default class Autolink extends React.PureComponent<AutolinkProps> {
-}
-
+export default class Autolink extends PureComponent<
+  AutolinkProps & TextProps
+> {}
