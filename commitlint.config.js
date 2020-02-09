1f94b1e43eaafcba8f418c35cbce351fd78492c5
Josh Swan <joshuawswan@gmail.com>
2020-02-08 19:26:50 -0800

    chore: configure commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,11 @@
+/*!
+ * React Native Autolink
+ *
+ * Copyright 2016-2020 Josh Swan
+ * Released under the MIT license
+ * https://github.com/joshswan/react-native-autolink/blob/master/LICENSE
+ */
+
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+};
